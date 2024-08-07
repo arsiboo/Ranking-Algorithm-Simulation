632f357052d033f0323fa6adbda51e1853e623a1
Arsineh Boodaghian Asl <arsineh.boodaghian@gmail.com>
2024-08-07 15:44:53 +0200

    Update PageRankSimulation_SingleInfluence.py

--- PageRankSimulation_SingleInfluence.py
+++ PageRankSimulation_SingleInfluence.py
@@ -1,50 +1,51 @@
-#!IMPORTANT please use pythong 3.8
 import networkx as nx
-import xlrd
+import openpyxl
 import xlsxwriter
 import numpy as np
+import os
 
-file = "Whole.xlsx"
+file = "updated_original.xlsx"
 
-_book = xlrd.open_workbook(file)
-_sheet = _book.sheet_by_index(1)
+_book = openpyxl.load_workbook(file)
+_sheet = _book.active
 
 allOutgoingNodes = list()
 noRepeatitionNodes = list()
 
-for row in range(_sheet.nrows):
-    if row > 0:
-        _data = _sheet.row_slice(row)
-        allOutgoingNodes.append(_data[0].value)
+for row in _sheet.iter_rows(min_row=2, values_only=True):
+    allOutgoingNodes.append(row[0])
 
 noRepeatitionNodes = list(dict.fromkeys(allOutgoingNodes))
 
+print(noRepeatitionNodes)
 flag = 1
 pr = None
+
+# Create directory if it doesn't exist
+output_dir = "simulation_output"
+os.makedirs(output_dir, exist_ok=True)
+
 for _nodes in noRepeatitionNodes:
     j = 1
     occurrences = allOutgoingNodes.count(_nodes)
-    workbook = xlsxwriter.Workbook("simulation_Output/" + _nodes + '_' + file)
+    workbook = xlsxwriter.Workbook(os.path.join(output_dir, f"{_nodes}_{file}"), {'nan_inf_to_errors': True})
     worksheet = workbook.add_worksheet()
 
     for w in np.arange(1, 100, 1):
         print(w)
         G = nx.DiGraph()
         weight = 0
-        for row in range(_sheet.nrows):
-            if row > 0:
-                _data = _sheet.row_slice(row)
-                _Node1 = _data[0].value
-                _Node2 = _data[1].value
-                if _Node2 == _nodes:
-                    weight = float(_data[2].value) * float(w)
-                elif _Node2 != _nodes:
-                    weight = float(_data[2].value)
-                G.add_weighted_edges_from([(_Node1, _Node2, weight)])
+        for row in _sheet.iter_rows(min_row=2, values_only=True):
+            _Node1 = row[0]
+            _Node2 = row[1]
+            if _Node2 == _nodes:
+                weight = float(row[3]) * float(w)
+            elif _Node2 != _nodes:
+                weight = float(row[3])
+            G.add_weighted_edges_from([(_Node1, _Node2, weight)])
 
         nodes_list = list(G.nodes())
         pr = nx.pagerank_numpy(G, alpha=0.65)
-        pr_list = list(pr)
         pr_keys = list(pr.keys())
         pr_values = list(pr.values())
         my_formatted_list = ['%.4f' % elem for elem in pr_values]
@@ -52,10 +53,11 @@ for _nodes in noRepeatitionNodes:
             for col_num, data in enumerate(pr_keys):
                 worksheet.write(0, col_num, data)
         for col_num, data in enumerate(my_formatted_list):
-            worksheet.write(j, col_num, float(data))
+            try:
+                worksheet.write(j, col_num, float(data))
+            except ValueError:
+                worksheet.write(j, col_num, 'NaN')
         j += 1
         G.clear()
-        # pr.clear()
         del G
-        # del pr
     workbook.close()
